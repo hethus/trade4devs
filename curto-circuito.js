1c1702a04950f8862b82e2d71f9bccc3267fce71
Giovanne Berteli Comba <joebcomba@gmail.com>
2022-04-12 22:25:20 -0300

    importante!

---
+++ curto-circuito.js
@@ -0,0 +1,5 @@
+// pode-se utilizar os verificadores && e || para retornar valores falsys ou trutys, exemplo:
+
+console.log('oi' && 'mundo' && 0 && 'teste'); // retorna o primeiro valor falso!
+
+console.log(0 || null || '' || 'oi' || 'mundo'); // retorna o primeiro valor verdadeiro!
