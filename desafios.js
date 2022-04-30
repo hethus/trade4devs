fab54a53824cdcde6747bd9c82747392e1fd3332
Giovanne Berteli Comba <joebcomba@gmail.com>
2022-04-30 12:57:05 -0300

    desafio

---
+++ desafios.js
@@ -0,0 +1,59 @@
+import enquirer from "enquirer";
+const { prompt } = enquirer;
+import chalk from 'chalk';
+console.clear();
+
+function calc(first, operador, second){
+    first = parseFloat(first);
+    second = parseFloat(second);
+    if(operador === '+'){
+        console.log(first + second);
+    }else if(operador === '-'){
+        console.log(first - second);
+    }else if(operador === '*'){
+        console.log(first * second);
+    }else if(operador === '/'){
+        console.log(first / second);
+    };
+};
+
+let first = await prompt({
+    type: 'input',
+    name: 'numero',
+    message: chalk.cyan('Primeiro valor: '),
+    validate: (value) => {
+        if (value != " " && value != null && !isNaN(value) && value != undefined && value != "") {
+          return true;
+        }
+        return "Digite um número válido!";
+      }
+});
+
+let math = await prompt([
+    {
+      type: "select",
+      name: "operacao",
+      message: chalk.cyan(`Qual operação deseja realizar?`),
+      initial: 0,
+      choices: [
+        { name: "+", message: `${chalk.red("Soma")}` },
+        { name: "-", message: `${chalk.green("Subtração")}` },
+        { name: "*", message: `${chalk.blue("Multiplicação")}` },
+        { name: "/", message: `${chalk.yellow("Divisão")}` }
+      ],
+    },
+]);
+
+let second = await prompt({
+    type: 'input',
+    name: 'numero',
+    message: chalk.cyan('Segundo valor: '),
+    validate: (value) => {
+        if (value != " " && value != null && !isNaN(value) && value != undefined && value != "") {
+          return true;
+        }
+        return "Digite um número válido!";
+      }
+});
+
+calc(first.numero, math.operacao, second.numero );
